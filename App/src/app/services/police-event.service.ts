c17037643c7dbfeb65abd92c77a7b1487b107477
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-02-10 19:58:37 +0100

    Docker for app

--- App/src/app/services/police-event.service.ts
+++ App/src/app/services/police-event.service.ts
@@ -10,7 +10,7 @@ import { LoggingService } from './logging.service';
     providedIn: 'root'
 })
 export class PoliceEventService {
-    private readonly BaseUrl = 'https://localhost:59006/events';
+    private readonly BaseUrl = 'https://192.168.1.186:5331/events'; //'https://localhost:59006/events';
 
     constructor(private readonly http: HttpClient, private readonly logger: LoggingService) {}
 
