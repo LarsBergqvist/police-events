390707c368a26e0252c6590abe37c5041b268025
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-02-24 19:42:08 +0100

    Moved logic from EventComponent to parent + modified spinner / isLoading behaviour

--- App/src/app/components/event-list/event-list.component.ts
+++ App/src/app/components/event-list/event-list.component.ts
@@ -5,6 +5,12 @@ import { MessageBrokerService } from 'src/app/services/message-broker.service';
 import { PoliceEventService } from 'src/app/services/police-event.service';
 import { getDateTimeNDaysFromNow, getUTCDateStringFromLocalDateTime } from 'src/app/utils/date-helper';
 import { GeoPosition } from 'src/app/models/geo-position';
+import { GeoJsonWrapper } from 'src/app/models/geojson-wrapper';
+import { LocationObjectViewModel } from 'src/app/models/location-object-viewmodel';
+import { AreaService } from 'src/app/services/area-service';
+import { NominatimService } from 'src/app/services/nominatim.service';
+import { LoggingService } from 'src/app/services/logging.service';
+import { ShowMapMessage } from 'src/app/messages/show-map.message';
 
 @Component({
     selector: 'app-event-list',
@@ -16,7 +22,13 @@ export class EventListComponent implements OnInit {
     keyword: string = '';
     isLoading = false;
 
-    constructor(private readonly service: PoliceEventService, private readonly broker: MessageBrokerService) {}
+    constructor(
+        private readonly service: PoliceEventService,
+        private readonly broker: MessageBrokerService,
+        private readonly areaService: AreaService,
+        private readonly nominatimService: NominatimService,
+        private readonly logger: LoggingService
+    ) {}
 
     async ngOnInit() {}
 
@@ -62,13 +74,13 @@ export class EventListComponent implements OnInit {
     }
 
     fetchMessageClose(distance: number) {
+        this.isLoading = true;
         navigator.geolocation.getCurrentPosition(async (position) => {
             const userPos: GeoPosition = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
             try {
-                this.isLoading = true;
                 const utcStrings = this.getUtcFromToStrings();
                 this.events = await this.service.fetchEventsForDateWithinRadius(
                     utcStrings[0],
@@ -82,6 +94,37 @@ export class EventListComponent implements OnInit {
                 this.isLoading = false;
             }
         });
+    }
+
+    async onShowOnMap(event: PoliceEventViewModel) {
+        let areaResult: GeoJsonWrapper;
+        let locationObject: LocationObjectViewModel;
+        try {
+            this.isLoading = true;
+            if (event.location.name.toLowerCase().includes('län')) {
+                // The location is a county
+                areaResult = await this.areaService.fetchGeoJsonForCounty(event.location.name);
+            } else {
+                // The location is a likely municipality
+                areaResult = await this.areaService.fetchGeoJsonForMunicipality(event.location.name);
+                // Make a text search for a more detailed location
+                locationObject = await this.nominatimService.searchBestMatchingLocationObject(
+                    event.location.name,
+                    event.summary,
+                    areaResult.boundingBox
+                );
+            }
+        } catch (error) {
+            this.logger.logError(error?.message);
+            const message = `Could not get map information for '${event.location.name}'`;
+            this.broker.sendMessage(new ErrorOccurredMessage(message));
+            return;
+        } finally {
+            this.isLoading = false;
+        }
+        const positions: GeoPosition[] = [];
+        positions.push(event.location.pos);
+        this.broker.sendMessage(new ShowMapMessage(event, areaResult, locationObject));
     }
 
     private getUtcFromToStrings(): [string, string] {
